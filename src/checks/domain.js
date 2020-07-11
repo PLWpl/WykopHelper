ab775399e30487873b9b9451dcb3d855585bdff1
PLWpl <kontakt@pwisniewski.pl>
2020-07-11 16:30:20 +0200

    added some domains

--- src/checks/domain.js
+++ src/checks/domain.js
@@ -1,14 +1,47 @@
 // add new domains. Always add them without protocol (http, https), www or path after domain.
-let domains = [
+export let rawDomains = [
   'lifesitenews.com',
-  'sputnik.com'
+  'alternews.pl',
+  'alexjones.pl',
+  'dziennik-polityczny.com',
+  'koniec-swiata.org',
+  'magnapolonia.org',
+  'narodowcy.net',
+  'nczas.com',
+  'ndie.pl',
+  'neon24.pl',
+  'newsweb.pl',
+  'parezja.pl',
+  'prostozmostu24.pl',
+  'prawdaobiektywna.pl',
+  'reporters.pl',
+  'sioe.pl',
+  'wmeritum.pl',
+  'wolnosc24.pl',
+  'wolna-polska.pl',
+  'wprawo.pl',
+  'wsensie.pl',
+  'zmianynaziemi.pl',
+  'sputniknews.com',
+  'rt.com',
+  'ruptly.tv',
+  'prawica.net',
+  'xportal.pl',
+  'kresy.pl',
+  'bdp.xportal.pl/',
+  'geopolityka.org',
+  'pravda.ru',
+  'voiceofrussia.com',
+  'ria.ru',
+  'ligakobietpolskich.pl',
+  'ronik.org.pl'
 ];
 
-domains.forEach(domain => {
-  domains.push('https://' + domain);
-  domains.push('https://www.' + domain);
-  domains.push('http://' + domain);
-  domains.push('http://www.' + domain);
+rawDomains.forEach(domain => {
+  rawDomains.push('https://' + domain);
+  rawDomains.push('https://www.' + domain);
+  rawDomains.push('http://' + domain);
+  rawDomains.push('http://www.' + domain);
 });
 
-export const russianPropagandaDomains = domains;
+export const russianPropagandaDomains = rawDomains;
