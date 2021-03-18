7e45e2b3094922021dc0a360191d096c7893789d
PLWpl <kontakt@pwisniewski.pl>
2021-03-19 00:09:36 +0100

    changelog updated

--- src/model/utils/update.js
+++ src/model/utils/update.js
@@ -2,6 +2,7 @@ import DOM from "../../constants/domSelectors";
 /* eslint max-len: 0 */
 
 const changesArray = [
+  'W ustawieniach można również od teraz eksportować i importować swoje ustawienia i listy oznaczonych użytkowników. Na razie jest to proces raczej ręczny (wymaga kopiowania i przeklejania ciągów znaków między przeglądarkami); możliwe, że w przyszłości coś tutaj zostanie udoskonalone, chociaż nie ukrywam, że wynika to z mojej niechęci do używania zewnętrznych usług - bo wtedy wchodziłyby w grę kwestie prywatności, dostępów, śledzenia i tak dalej i tak dalej... a tego chcę za wszelką cenę uniknąć.',
   'Od teraz w ustawieniach można wybrać domyślny kolor odznaki. Wkrótce pojawi się możliwość ustawiania osobnego koloru dla każdego oznaczonego użytkownika.',
   'Zniknęło sporo pomniejszych bugów.',
   'Z pewnością pojawiło się sporo nowych bugów :)'
